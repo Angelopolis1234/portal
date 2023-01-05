5ab9fbc4343e00d6f52cd7440a4b26949b680112
Angel <angel.maldonado.garcia.1100@gmail.com>
2023-01-04 19:37:38 -0600

    Agregando funcionalidades de empleado

--- main.js
+++ main.js
@@ -130,3 +130,80 @@ app.get('/editProduct', (req, res) => {
 	}
 });
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Administracion de empleados
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.get('/addEmpleado', (req, res) => {
+	let data = {
+		nombre: req.query.nombre,
+		apellidop: req.query.apellidop,
+		apellidom: req.query.apellidom,
+		direccion: req.query.direccion,
+		telefono: req.query.telefono,
+		correo: req.query.correo,
+		salario: req.query.salario,
+		cargo: req.query.cargo,
+		ingreso: req.query.ingreso,
+		horario: req.query.horario,
+		password: req.query.password
+	}
+	console.log(`[API] :: Data fetched: ${data.nombre}, ${data.apellidop}, ${data.apellidom}, ${data.direccion}, ${data.telefono}, ${data.correo}, ${data.salario}, ${data.cargo}, ${data.ingreso}, ${data.horario}`);
+	if (data.password === password) {
+		const sql = `INSERT INTO empleado(nombre,apellido_paterno,apellido_materno,direccion,telefono,correo,salario,cargo,fecha_ingreso,id_horario) VALUES (\'${nombre}\',\'${apellidop}\',\'${apellidom}\',\'${direccion}\',\'${telefono}\',\'${correo}\',${salario},\'${cargo}\',\'${decha_ingreso}\',${horario}')`
+		console.log(`[API] :: QUERY -> ${sql}`);
+		connection.query(sql, (err, results) => {
+			if (err) throw err;
+			else res.send('Empleado Agregado');
+		});
+	}else{
+		res.send('Incorrect Password');
+	}
+});
+
+app.get('/editEmpleado', (req, res) => {
+	let data = {
+		id: req.query.id,
+		nombre: req.query.nombre,
+		apellidop: req.query.apellidop,
+		apellidom: req.query.apellidom,
+		direccion: req.query.direccion,
+		telefono: req.query.telefono,
+		correo: req.query.correo,
+		salario: req.query.salario,
+		cargo: req.query.cargo,
+		ingreso: req.query.ingreso,
+		horario: req.query.horario,
+		password: req.query.password
+	}
+	console.log(`[API] :: Data fetched: ${data.id}, ${data.nombre}, ${data.apellidop}, ${data.apellidom}, ${data.direccion}, ${data.telefono}, ${data.correo}, ${data.salario}, ${data.cargo}, ${data.ingreso}, ${data.horario}`);
+	if (data.password === password) {
+		const sql = `UPDATE empleado SET nombre=\'${data.nombre}\', apellido_paterno=\'${data.apellidop}\', apellido_materno=\'${data.apellidom}\', direccion=\'${data.direccion}\', telefono=\'${data.telefono}\' ,correo=\'${data.correo}\', salario=${data.salario}, cargo=\'${data.cargo}\', fecha_ingreso=\'${data.ingreso}\',id_horario=${data.horario} WHERE num_empleado=${data.id}`;
+		console.log(`[API] :: QUERY -> ${sql}`);
+		connection.query(sql, (err, results) => {
+			if (err) throw err;
+			else res.send('Empleado Editado Correctamente');
+		});
+	}else{
+		res.send('Incorrect Password');
+	}
+});
+
+app.get('/deleteEmpleado', (req, res) => {
+	let data = {
+		id: req.query.id,
+		password: req.query.password
+	}
+	console.log(`[API] :: Data fetched: ${data.id}`);
+	if (data.password === password) {
+		const sql = `DELETE FROM empleado WHERE num_empleado=${data.id}`;
+		console.log(`[API] :: QUERY -> ${sql}`);
+		connection.query(sql, (err, results) => {
+			if(err) throw err;
+			else res.send('Empleado eliminado	correctamente');
+		});
+	}else{
+		res.send('Incorrect Password');
+	}
+});
