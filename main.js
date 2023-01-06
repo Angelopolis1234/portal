d90f238ee539366492f538a4a2c7ad50e8d37a16
Angel <angel.maldonado.garcia.1100@gmail.com>
2023-01-06 12:51:37 -0600

    Configurando post

--- main.js
+++ main.js
@@ -240,20 +240,28 @@ const getIdOrden = (query) => {
 		});
 	})
 }
+
 app.post('/ordenar', (req, res) => {
 	console.log(`[API]:: datos obtenidos: ${JSON.stringify(req.body)}`);
 	let data = req.body;
 	let query = `INSERT INTO orden(hora,mesa,personas,mesero) VALUES (\"${data[data.length - 1].tiempo}\",${data[data.length - 1].mesa},${data[data.length - 1].personas},${data[data.length - 1].mesero})`;
 	let ids = data.map(item => item.id_producto);
-	query = `SELECT num_orden FROM orden WHERE mesa=3 AND mesero=1 AND hora=\'2022-12-20 11:50:00\'`;
+	connection.query(query, (err, results) => {
+		if (err) throw err;
+	});
+	query = `SELECT num_orden FROM orden WHERE mesa=${data[data.length - 1].mesa} AND mesero=${data[data.length - 1].mesero} AND hora=\'${data[data.length - 1].tiempo}\'`;
 	let id_orden;
 	getIdOrden(query)
 	.then(result => {
-		id_orden = result;
-		console.log(result[0]);
-		console.log(id_orden);
+		id_orden = result[0];
+		for (let i = 0; i < ids.length; i++) {
+			query = `INSERT INTO producto_to_orden(id_producto,num_orden) VALUES (${ids[i]},${data[data.length - 1].num_orden})`;
+			console.log(`[API] :: QUERY -> ${query}`);
+			connection.query(query, (err, result) => {
+				if (err) throw err;
+			});
+		}
 	})
-	console.log(`[API] :: QUERY -> ${query}`);
 	/*connection.query(query, (err, results) => {
 		if (err) throw err;
 	});
